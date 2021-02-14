ff952cc5e490500b1683e2dd3a0c171b4c9aee1c
SeMurA823 <artyom.muraweow2018@yandex.ru>
2021-02-15 00:01:37 +0400

    fix arrays.js

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -6,11 +6,11 @@ let array = [
     document.getElementById('arr5')
 ];
 let statistic = () => {
-    let min = array[0].value, max = array[0].value;
+    let min = Number(array[0].value), max = Number(array[0].value);
     console.log(array[0].value)
     for (let i = 1; i < array.length; i++) {
-        if (min > Number.parseInt(array[i].value)) min = Number.parseInt(array[i].value);
-        if (max < Number.parseInt(array[i].value)) max = Number.parseInt(array[i].value);
+        if (min > Number(array[i].value)) min = Number(array[i].value);
+        if (max < Number(array[i].value)) max = Number(array[i].value);
     }
     document.getElementById('max-arr').textContent = max;
     document.getElementById('min-arr').textContent = min;
