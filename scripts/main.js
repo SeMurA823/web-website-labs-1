57f422cc8a885ef23b146ac0f5ef05afef47dd42
SeMurA823 <artyom.muraweow2018@yandex.ru>
2021-02-14 21:00:49 +0400

    add animated background

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+let examplesList = document.getElementsByClassName('adaptive-background');
+let offsetExamplesList = [];
+for (let i = 0; i < examplesList.length; i++){
+    offsetExamplesList.push(examplesList.item(i).offsetTop);
+}
+console.log(offsetExamplesList)
+
+let oldIndexBlock = 0;
+
+updateColorBlock();
+
+function getIndexBlock(){
+    console.log(window.pageYOffset);
+    let indexBlock = 0;
+    for (let i = 0; i < examplesList.length; i++) {
+        console.log(examplesList.item(i).clientHeight);
+        if (window.pageYOffset + window.innerHeight / 2 >= examplesList.item(i).offsetTop && window.pageYOffset + window.innerHeight / 2 <= examplesList.item(i).offsetTop + examplesList.item(i).clientHeight) {
+            console.log("I = " + i);
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+function updateColorBlock(){
+    let indexBlock = getIndexBlock();
+    if (oldIndexBlock !== indexBlock) {
+        examplesList.item(oldIndexBlock).style.opacity = '0.0';
+        examplesList.item(indexBlock).style.opacity = '1.0';
+        oldIndexBlock = indexBlock;
+    }
+    if (indexBlock < 0) {
+        document.body.style.backgroundColor = 'white';
+    } else {
+        document.body.style.backgroundColor = examplesList.item(indexBlock).getAttribute('data-background');
+    }
+}
+
+window.addEventListener('scroll',()=>{
+    updateColorBlock();
+});
