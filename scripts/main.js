d7c8bb1b82f87eb1e866624fb2691a4498f879b4
SeMurA823 <artyom.muraweow2018@yandex.ru>
2021-02-19 11:07:18 +0400

    add helper-window

--- scripts/main.js
+++ scripts/main.js
@@ -38,9 +38,49 @@ updateColorBlock();
 
 window.addEventListener('scroll',updateColorBlock);
 
-function viewHelp(id) {
-    let block = document.getElementById(id);
-    block.style.display = 'flex';
+// словарь подсказок
+let mapHelpText = new Map();
+mapHelpText.set('modal-window',
+    "Для реализации модального окна использовался метод 'alert'," +
+    "для сохранения имени в локальное хранилище (localStorage.setItem('user',name))."
+);
+mapHelpText.set('triangle',
+    "Для вычисления применяются алгебраические операции / и *. " +
+    "Для начала вычисления срабатывает по событию 'change' в полях ввода"
+);
+mapHelpText.set('conditional-operator',
+    "Для вычисления применяются операторы === и !==. " +
+    "Для начала вычисления, срабатывает по событию 'change' в полях ввода"
+);
+mapHelpText.set('arrays',
+    "В массиве элементов находится минимальный и максимальный элемент " +
+    "при помощи алгоритма поиска min и max в массиве. " +
+    "Для начала вычисления, срабатывает по событию 'click' на кнопку"
+);
+mapHelpText.set('timer',
+    "Для реализации таймера применяется функция timeId = setInterval(/*функция*/,/*интервал*/), " +
+    "циклически вызывающая функцию. " +
+    "Для остановки таймера использована clearInterval(timeId), останавлиющая циклическое выполнение функции. " +
+    "Для сброса таймера обнуляется значение времени"
+);
+mapHelpText.set('test',
+    "Для реализации теста использовалась методы DOM: document.createElement(/*тег*/). " +
+    "Для реализации хранения вопросов и ответов используется массив вопросов и соответственно ответов к ним. " +
+    "Для реализации проверки ответов, циклом итерируемся по представлению ответов и самим ответам, если ответ не верный, " +
+    "то ответ \"краснеет\", иначе \"зеленеет\""
+)
+mapHelpText.set('screensaver',
+    "Для реализации заставки использовалось изменение стиля припомощи своств элемента. " +
+    "Для запуска заставки использовалась событие 'click' по кнопке. " +
+    "Для остановки заставки использовалась событие 'click' по области документа"
+);
+
+function viewHelp(element) {
+    let id = element.parentNode.parentElement.id;
+    document.getElementById('help').style.display = 'flex';
+    document.getElementById('help-text').textContent = mapHelpText.get(id);
 }
+
 function closeHelp() {
+    document.getElementById('help').style.display = 'none';
 }
